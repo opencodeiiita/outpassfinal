78b0c71ec59fc63b99d812f5850f51f2d5cb9063
pradeepgangwar <pradeepgangwar39@gmail.com>
2019-01-13 12:00:19 +0530

    Change directory structure

--- pass/settings.py
+++ pass/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'leave',
-    'pass.core',
+    'core',
 ]
 
 MIDDLEWARE = [
--- pass/urls.py
+++ pass/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.contrib.auth import views
 from django.conf.urls import url, include
 
+from core import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
